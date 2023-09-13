92836e3e9558aed5fe069fbc9e08981deeabf2a4
Tavaana12 <144510396+Tavaana12@users.noreply.github.com>
2023-09-13 23:45:00 +0430

    Registry soldier with c language
    If you have better way sharing with me...

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    char singles;
+    char gander;
+    int age;
+
+
+
+    printf("Enter S for : \n");
+    scanf("%c",&singles);
+    if (singles == 's'){
+        prinf("your Ok");
+    }
+    printf("Enter your gander with  M for man : \n");
+    scanf("%c",&gander);
+    if ( gander == 'g'){
+        printf("your Ok");
+    }
+    printf("Enter your age :\n");
+    scanf("%d",&age);
+
+
+
+
+
+
+
+
+    return 0;
+}
